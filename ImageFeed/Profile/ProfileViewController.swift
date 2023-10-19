ab2c0e5ffd1ca86ab68685a7bbf0b38f5c59b599
AlbertKhaydarov <aghaydarov@gmail.com>
2023-10-19 15:03:09 +0300

    add ProfileViewController, add Labels, Button, ImageView and its constraints on Storyboard in screen Profile View Controller, add IBOutlets and IBAction

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Альберт Хайдаров on 19.10.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak private var userPickImageView: UIImageView!
+    
+    @IBOutlet weak private var namelabel: UILabel!
+    
+    @IBOutlet weak private var userNameLabel: UILabel!
+    
+    @IBOutlet weak private var statusLabel: UILabel!
+    
+    @IBOutlet weak private var logoutButton: UIButton!
+    
+    @IBAction private func logoutButtonTapped(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+
+}
