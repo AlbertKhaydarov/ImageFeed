fb46441338cb259950f01b625f08a2b6d169480d
AlbertKhaydarov <aghaydarov@gmail.com>
2023-10-19 16:45:45 +0300

    add SingleImageViewController

---
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Альберт Хайдаров on 19.10.2023.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageVie: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+  
+
+}
