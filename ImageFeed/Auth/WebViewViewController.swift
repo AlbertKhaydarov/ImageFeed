8fb2318924c7e09d3d2c3f78ac4623122c986e37
AlbertKhaydarov <aghaydarov@gmail.com>
2023-11-07 12:28:43 +0300

    add WebViewViewController and WebViewViewControllerScene in StoryBoard

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Альберт Хайдаров on 07.11.2023.
+//
+
+import UIKit
+import WebKit
+
+
+class WebViewViewController: UIViewController {
+
+    @IBOutlet private var webView: WKWebView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction private func didTapBackButton(_ sender: UIButton) {
+    }
+    
+
+}
