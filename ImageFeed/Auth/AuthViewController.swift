43b42f1f4001b9d31e18e9f8536973fc3963d20a
AlbertKhaydarov <aghaydarov@gmail.com>
2023-11-07 11:34:21 +0300

    add AuthViewController + AuthViewControllerScene in Storyboard

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Альберт Хайдаров on 07.11.2023.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
