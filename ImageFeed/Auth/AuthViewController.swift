4a998b1443785639a53ff00e63d73feee14436a8
AlbertKhaydarov <aghaydarov@gmail.com>
2023-11-07 14:08:01 +0300

    add protocol WebViewViewControllerDelegate and its implementation in AuthViewController

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 class AuthViewController: UIViewController {
     
     let showWebViewSegueIdentifier = "ShowWebView"
-
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
+    @IBAction func signInButtonTapped(_ sender: UIButton) {
+     
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewSegueIdentifier {
+            guard let destinationVC = segue.destination as? WebViewViewController else { return }
+                destinationVC.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+}
 
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -12,15 +12,54 @@ import WebKit
 class WebViewViewController: UIViewController {
 
     @IBOutlet private var webView: WKWebView!
-   
+    
+  private let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    
+    weak var delegate: WebViewViewControllerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+           URLQueryItem(name: "client_id", value: AccessKey),
+           URLQueryItem(name: "redirect_uri", value: RedirectURI),
+           URLQueryItem(name: "response_type", value: "code"),
+           URLQueryItem(name: "scope", value: AccessScope)
+         ]
+         let url = urlComponents.url!
+       
+        let request = URLRequest(url: url)
+ 
+        webView.load(request)
+        webView.navigationDelegate = self
     }
     
     @IBAction private func didTapBackButton(_ sender: UIButton) {
+        delegate?.webViewViewControllerDidCancel(self)
     }
     
+}
 
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let code = code(from: navigationAction) {
+            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+    
+    private func code(from navigationAction: WKNavigationAction) -> String? {
+        if let url = navigationAction.request.url,
+           let urlComponents = URLComponents(string: url.absoluteString),
+           urlComponents.path == "/oauth/authorize/native",
+           let items = urlComponents.queryItems,
+           let codeItem = items.first(where: {$0.name == "code"})
+        {
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
 }
