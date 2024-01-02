616ff7c110814da0cfbd833e63a374efd6181212
AlbertKhaydarov <aghaydarov@gmail.com>
2024-01-02 21:19:16 +0300

    edit

---
+++ ImageFeedUIlikedTests/ImageFeedUIlikedTests.swift
@@ -0,0 +1,46 @@
+//
+//  ImageFeedUIlikedTests.swift
+//  ImageFeedUIlikedTests
+//
+//  Created by Альберт Хайдаров on 02.01.2024.
+//
+import Foundation
+import XCTest
+@testable import ImageFeed
+
+final class ImageFeedUIlikedTests: XCTestCase {
+    private let app = XCUIApplication()
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        
+        app.launch()
+    }
+    
+    func testFeed() throws {
+     
+        let tablesQuery = app.tables
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        sleep(8)
+        cell.swipeUp()
+        
+        sleep(10)
+        
+        let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
+        let favoriteActiveButton = cellToLike.buttons["favoriteActiveButton"]
+        favoriteActiveButton.tap()
+        sleep(5)
+        favoriteActiveButton.tap()
+        sleep(5)
+        
+        cellToLike.tap()
+        sleep(5)
+        
+        let image = app.scrollViews.images.element(boundBy: 0)
+        image.pinch(withScale: 3, velocity: 1)
+        image.pinch(withScale: 0.5, velocity: -1)
+        
+        let navBackButton = app.buttons["navBackButton"]
+        
+    }
+}
