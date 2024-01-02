b68c76915296ec57c9f405189dde7880b1fc552f
AlbertKhaydarov <aghaydarov@gmail.com>
2024-01-02 21:24:42 +0300

    remove Test Constants

---
+++ ImageFeedUITests/TestConstants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeedUITests
+//
+//  Created by Альберт Хайдаров on 30.12.2023.
+//
+
+import Foundation
+
+enum TestConstants {
+    static let login = ""
+    static let password = ""
+    static let fullName = ""
+    static let username = ""
+    
+}
